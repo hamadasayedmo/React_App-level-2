919968277d322cdd593ef58bbf99147ecb529cab
hamadasayedmo <hh068506hamada@gmail.com>
2024-10-24 16:39:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+// use Context
+import { useContext } from "react";
+import ThemeProvider from "./context/ContextData";
+
+// Start Routing Code
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/home";
+import Html from "./pages/html";
+import Css from "./pages/css";
+import Js from "./pages/javascript";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>Error</h1>,
+  },
+  {
+    path: "/html",
+    element: <Html />,
+  },
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Js />,
+  },
+]);
+// End Routing Code
+
+const App = () => {
+  // use Context
+  const { theme } = useContext(ThemeProvider);
+
+  return (
+    <div className={`${theme}`}>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
